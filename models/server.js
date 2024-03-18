effe10b855b56b254198a6ba925e1155b91fa087
wilsonvodka <74803684+wilsonvodka@users.noreply.github.com>
2024-03-18 00:10:22 -0600

    Update server.js
    port agregado

--- models/server.js
+++ models/server.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 class Server {
     constructor() {
         this.app = express()
-        this.port = process.env.PORT
+        this.port = process.env.PORT || 8080
         this.usuariosRoutePath = '/api/usuarios'
 
         //Middlewares
